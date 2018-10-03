7e66abce20b1f7fd2fe63c1f445063559db13c15
Víctor <v.escudero99@gmail.com>
2018-10-03 12:18:57 +0200

    cmd_pid añadido

--- main.c
+++ main.c
@@ -7,79 +7,91 @@
 #define clear() printf("\033[H\033[J")
 
 int trocearCadena ( char * cadena, char * trozos[]){
+	int  i = 1;
 
-    int  i = 1;
-
-    if((trozos[0] = strtok(cadena, " \n\t")) == NULL)
-        return 0;
-    while ((trozos[i]=strtok(NULL, " \n\t")) != NULL)
-        i++;
-    return i;
+	if((trozos[0] = strtok(cadena, " \n\t")) == NULL)
+		return 0;
+	while ((trozos[i]=strtok(NULL, " \n\t")) != NULL)
+		i++;
+	return i;
 
 }
 
 void imprimirPrompt(int counter){
-    printf("\n $ ");
+	printf("\n $ ");
 }
 
 void leerEntrada( char * cadena){
-    fgets(cadena, 1024, stdin);
+	fgets(cadena, 1024, stdin);
 }
 
 int cmd_autores(char * flags[], int nargs){
+	int i = 1;
+	int valid = 1;
 
-    int i = 1;
-    int valid = 1;
-
-    char *autores[2] = {"Manuel Couto Pintos","Victor Escudero Gonzalez"};
-    char *correos[2] = {"manuel.couto1@udc.es", "victor.escudero@udc.es"};
-    char *salida = malloc(1024);
+	char *autores[2] = {"Manuel Couto Pintos","Victor Escudero Gonzalez"};
+	char *correos[2] = {"manuel.couto1@udc.es", "victor.escudero@udc.es"};
+	char *salida = malloc(1024);
 
 
-    if(nargs == 1){
+	if(nargs == 1){
 
-        strcpy(salida, autores[0]);
-        strcat(salida, "\t\t\t");
-        strcat(salida, autores[1]);
-        strcat(salida,"\n\t");
-        strcat(salida, correos[0]);
-        strcat(salida, "\t\t");
-        strcat(salida, correos[1]);
-        //strcat(salida,"\n\t");
-        printf("%s",salida);
-    } else if(nargs < 4){
-    	strcpy(salida,"");
-        while(i < nargs && valid){
-            if(!strcmp(flags[i],"-n")){
-                strcat(salida, autores[0]);
-                strcat(salida, "\t\t\t");
-                strcat(salida, autores[1]);
-            } else if(!strcmp(flags[i],"-l")){
-                strcat(salida, correos[0]);
-                strcat(salida, "\t\t");
-                strcat(salida, correos[1]);
-            } else {
-                valid = 0 ;;
-                printf("ERROR invalid command");
-            }
-            i++;
-            if(i<nargs) strcat(salida,"\n\t");
-        }
-        printf("%s", salida);
-    } else {
-    	printf("ERROR invalid command");
-    }
-    free(salida);
-    return 0;
+		strcpy(salida, autores[0]);
+		strcat(salida, "\t\t\t");
+		strcat(salida, autores[1]);
+		strcat(salida,"\n\t");
+		strcat(salida, correos[0]);
+		strcat(salida, "\t\t");
+		strcat(salida, correos[1]);
+		//strcat(salida,"\n\t");
+		printf("%s",salida);
+		} else if(nargs < 4){
+		strcpy(salida,"");
+		while(i < nargs && valid){
+			if(!strcmp(flags[i],"-n")){
+				strcat(salida, autores[0]);
+				strcat(salida, "\t\t\t");
+				strcat(salida, autores[1]);
+			} else if(!strcmp(flags[i],"-l")) {
+				strcat(salida, correos[0]);
+				strcat(salida, "\t\t");
+				strcat(salida, correos[1]);
+			} else {
+				valid = 0 ;;
+				printf("ERROR invalid command");
+			}
+			i++;
+			if(i<nargs) strcat(salida,"\n\t");
+		}
+		printf("%s", salida);
+	} else {
+		printf("ERROR invalid command");
+	}
+	free(salida);
+	return 0;
 
 }
 
-int cmd_pid(char * flags[], int nargs){
-	return 0;
+void cmd_pid(char * flags[], int num) {
+	switch (num) {
+		case 1:
+			printf("Process ID: %d\n", getpid());
+			break;
+		case 2:
+			if (!strcmp(flags[1],"-p")) {
+				printf("Parent Process ID: %d\n", getppid());
+			}
+			else {
+				printf("Error\n");
+			}
+			break;
+		default:
+			printf("Error\n");
+	}
+
 }
 
 int cmd_chdir(char * flags[], int nargs){
-
 	char dir[1024];
 
 	if(nargs == 1){
@@ -126,28 +138,28 @@ int cmdManager(char *trozos[], int ntrozos){
 
 int procesarEntrada(char * cadena){
 
-    char * trozos [1024];
-    int ntrozos;
+	char * trozos [1024];
+	int ntrozos;
 
-    if((ntrozos = trocearCadena(cadena, trozos))){
-    	printf("\t");
-        return cmdManager(trozos,ntrozos);
-        //printf("\n");
-    }
-    return 0;
+	if((ntrozos = trocearCadena(cadena, trozos))){
+		printf("\t");
+		return cmdManager(trozos,ntrozos);
+		//printf("\n");
+	}
+	return 0;
 }
 
 int main() {
-    clear();
-    char * entrada ;
-    int salir = 0;
-    int counter = 1;
+	clear();
+	char * entrada ;
+	int salir = 0;
+	int counter = 1;
 	entrada = malloc(1024);
-    while (!salir) {
-        imprimirPrompt(counter);
-        leerEntrada(entrada);
-        salir = procesarEntrada(entrada);
-        counter++;
-    }
+	while (!salir) {
+		imprimirPrompt(counter);
+		leerEntrada(entrada);
+		salir = procesarEntrada(entrada);
+		counter++;
+	}
 	free(entrada);
 }
