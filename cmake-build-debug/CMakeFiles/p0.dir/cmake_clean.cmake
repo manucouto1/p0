6fa82202430300fee333e83549244eaf8f4814e4
manucouto1 <manu.couto1k@gmail.com>
2018-11-15 12:19:34 +0100

    test branch

---
+++ cmake-build-debug/CMakeFiles/p0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p0.dir/list.c.o"
+  "p0.pdb"
+  "p0.exe"
+  "libp0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
